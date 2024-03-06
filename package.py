d24dff06c05d3d12b0b6e0b070f4730770b5a2a1
Cooper <icooper@hendrickson-intl.com>
2024-03-06 08:35:29 -0500

    import package class into main

--- package.py
+++ package.py
@@ -8,8 +8,3 @@ class Package:
         self.deadline = deadline
         self.weight = weight
         self.notes = notes
-
-
-p1 = Package(1, "195 W Oakland Ave" , "Salt Lake City" , "UT" , "84115" , "10:30 AM",21, "test note")
-
-print(p1.notes)
