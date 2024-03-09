2bd64cd78560848648e56362febf4eeadb1be376
Ian Cooper <icoop17@wgu.edu>
2024-03-09 10:24:28 -0500

    finish implimentation

--- hash_tutorial/hashtable.py
+++ hash_tutorial/hashtable.py
@@ -8,16 +8,16 @@ class HashTable:
     def __init__(self, capacity):
         if capacity < 1:
             raise ValueError("Capacity must be a positive number")
-        self._pairs = capacity * [None]
+        self._slots = capacity * [None]
 
     def __len__(self):
         return len(self.pairs)
 
     def __setitem__(self, key, value):
-        self._pairs[self._index(key)] = Pair(key, value)
+        self._slots[self._index(key)] = Pair(key, value)
 
     def __getitem__(self,key):
-        pair = self._pairs[self._index(key)]
+        pair = self._slots[self._index(key)]
         if pair is None:
             raise KeyError(key)
         return pair.value
@@ -32,9 +32,32 @@ class HashTable:
 
     def __delitem__(self,key):
         if key in self:
-            self._pairs[self._index(key)] = None
+            self._slots[self._index(key)] = None
         else:
             raise KeyError(key)
+        
+    def __iter__(self):
+        yield from self.keys
+
+    def __str__(self):
+        pairs = []
+        for key, value in self.pairs:
+            pairs.append(f"{key!r}: {value!r}")
+        return "{" + ", ".join(pairs) + "}"
+    
+    def __repr__(self):
+        cls = self.__class__.__name__
+        return f"{cls}.from_dict({str(self)})"
+    
+    def  __eq__(self, other):
+        if self is other:
+            return True
+        if type(self) is not type(other):
+            return False
+        return set(self.pairs) == set(other.pairs)
+    
+    def copy(self):
+        return HashTable.from_dict(dict(self.pairs), self.capacity)
 
     def get(self, key, default=None):
         try:
@@ -44,7 +67,7 @@ class HashTable:
 
     @property
     def pairs(self):
-        return [pair for pair in self._pairs if pair]
+        return [pair for pair in self._slots if pair]
     
     @property
     def values(self):
@@ -56,11 +79,18 @@ class HashTable:
     
     @property
     def pairs(self):
-        return{pair for pair in self._pairs if pair}
+        return{pair for pair in self._slots if pair}
     
     @property
     def capacity(self):
-        return len(self._pairs)
+        return len(self._slots)
+    
+    @classmethod
+    def from_dict(cls, dictionary, capacity=None):
+        hash_table = cls(capacity or len(dictionary) * 10)
+        for key, value in dictionary.items():
+            hash_table[key] = value
+        return hash_table
 
     def _index(self, key):
         return hash(key) % self.capacity
