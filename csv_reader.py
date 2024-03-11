145d5e7ccff1dae99f5efe4fc85ef432cdf3b406
Ian Cooper <icooper@hendrickson-intl.com>
2024-03-11 07:25:02 -0400

    move package creationm from csv to csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -2,22 +2,23 @@ import csv
 from package import Package
 from hash_table import HashTable
 
-package_hash = HashTable(capacity=41)
+def package_hash_from_csv(file_path):
+    package_hash = HashTable(capacity=41)
 
-with open('data\packages.csv') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    next(csv_reader) #skip header row
-    for row in csv_reader:
-        package_id = int(row[0])
-        address = row[1]
-        city = row[2]
-        state = row[3]
-        zip = row[4]
-        delivery_deadline = row[5]
-        weight = row[6]
-        notes = row[7]
+    with open(file_path) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader) #skip header row
+        for row in csv_reader:
+            package_id = int(row[0])
+            address = row[1]
+            city = row[2]
+            state = row[3]
+            zip = row[4]
+            delivery_deadline = row[5]
+            weight = row[6]
+            notes = row[7]
 
-        working_package = Package(package_id, address, city, state, zip, delivery_deadline, weight, notes)
-        package_hash[package_id] = working_package
+            working_package = Package(package_id, address, city, state, zip, delivery_deadline, weight, notes)
+            package_hash[package_id] = working_package
 
-print(package_hash[15])
+    return package_hash
--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from package import Package
 from hash_table import HashTable
+from csv_reader import package_hash_from_csv
 
-package_hash = HashTable(capacity=40)
-
-p1 = Package(1, "195 W Oakland Ave", "Salt Lake City", "UT", "84115", "10:30 AM", "21")
-
-p2 = Package(2,"2530 S 500 E","Salt Lake City","UT","84106","EOD","44")
-
-package_hash[1] = p1
-package_hash[2] = p2
+package_hash = package_hash_from_csv('data\packages.csv')
 
 print(package_hash[1].package_id)
 
