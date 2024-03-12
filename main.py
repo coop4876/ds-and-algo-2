4e157724f65333fa940953adab2d2c588fb6bff7
Ian Cooper <icooper@hendrickson-intl.com>
2024-03-12 07:35:17 -0400

    fix data file path

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from package import Package
 from location import Location
 
-package_file = 'data\packages.csv'
-location_file = 'data\location.csv'
-distance_file = 'data\distance.csv'
+package_file = 'data/packages.csv'
+location_file = 'data/location.csv'
+distance_file = 'data/distance.csv'
 
 location_hash = Location.build_distance_and_location_hash(location_file, distance_file)
 package_hash = Package.build_package_hash_from_csv(package_file)
