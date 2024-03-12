052846bcbcf93ee2418aba0d5f5e840a72f6af76
Ian Cooper <icoop17@wgu.edu>
2024-03-11 22:34:44 -0400

    continue working on location module

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from package import Package
 from location import Location
-from hash_table import HashTable
 
-package_location = 'data\packages.csv'
+package_file = 'data\packages.csv'
+location_file = 'data\location.csv'
+distance_file = 'data\distance.csv'
 
-l1 = Location("HUB")
-l2 = Location("1060 Dalton Ave S (84104)")
+location_hash = Location.build_distance_and_location_hash(location_file, distance_file)
+package_hash = Package.build_package_hash_from_csv(package_file)
 
-print(l1.address)
-print(l2.address)
+print(package_hash[0].address)
+print(location_hash[0].address)
 
-
-
-# package_hash = Package.build_package_hash_from_csv(package_location)
-
-
-# print(package_hash[0].address)
-
-# print(package_hash[1].address)
+print(package_hash[1].address)
+print(location_hash[1].address)
