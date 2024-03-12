6fc875773ad4dad9810f8c1a8e371b76933ddfa9
Ian Cooper <icooper@hendrickson-intl.com>
2024-03-12 09:09:02 -0400

    file cleanup

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from package import Package
-from location import Location
 
 package_file = 'data/packages.csv'
 location_file = 'data/location.csv'
 distance_file = 'data/distance.csv'
 
-location_hash = Location.build_distance_and_location_hash(location_file, distance_file)
 package_hash = Package.build_package_hash_from_csv(package_file)
 
 # print(package_hash[0].address)
@@ -17,8 +15,3 @@ package_hash = Package.build_package_hash_from_csv(package_file)
 
 for x in range(len(package_hash)):
     print(package_hash[x].address + ' ' + package_hash[x].zip)
-
-print('***********************************')
-
-for x in range(len(location_hash)):
-    print(location_hash[x].address)
