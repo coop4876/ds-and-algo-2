5de845e25db2830b9e0ad7da76b5e5bd63bedcac
Ian Cooper <iancooper2010@gmail.com>
2024-03-12 21:55:31 -0400

    create location_and_distance module

--- location_and_distance.py
+++ location_and_distance.py
@@ -1,8 +1,46 @@
 import csv
 
 
-class Location:
-    pass
+class DataLoader:
+    def __init__(self, location_file_path, distance_file_path):
+        self.location_file_path = location_file_path
+        self.distance_file_path = distance_file_path
+        self.location_data = None
+        self.distance_data = None
+        
 
-class Distance:
-    pass
+    def load_data(self):
+        # with open(self.location_file_path, 'r') as location_file:
+        #     self.location_data = list(csv.reader(location_file, delimiter=','))
+
+        # with open(self.distance_file_path, 'r') as distance_file:
+        #     self.distance_data = list(csv.reader(distance_file, delimiter=','))
+        location_file = open(self.location_file_path, 'r')
+        self.location_data = list(csv.reader(location_file, delimiter=','))
+        location_file.close
+
+        distance_file = open(self.distance_file_path, 'r')
+        self.distance_data = list(csv.reader(distance_file, delimiter=','))
+        distance_file.close
+
+
+class DistanceCalculator:
+    def __init__(self, location_data, distance_data):
+        self.location_data = location_data
+        self.distance_data = distance_data
+
+    def get_distance(self, start_address, end_address):
+        start_index = None
+        end_index = None
+        for current_index in range(len(self.location_data)):
+            if self.location_data[current_index][1] == start_address:
+                start_index = current_index
+            if self.location_data[current_index][1] == end_address:
+                end_index = current_index
+
+        if self.distance_data[start_index][end_index] == '':
+            distance = self.distance_data[end_index][start_index]
+        else:
+            distance = self.distance_data[start_index[end_index]]
+
+        return distance
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from package import Package
+from location_and_distance import DataLoader, DistanceCalculator
+
+location_file_path = 'data/location.csv'
+distance_file_path = 'data/distance.csv'
+
+data_loader = DataLoader(location_file_path, distance_file_path)
+data_loader.load_data()
+
+distance_calculator = DistanceCalculator(data_loader.location_data, data_loader.distance_data)
+
+start_address = '1060 Dalton Ave S (84104)'
+end_address = '195 W Oakland Ave (84115)'
+
+distance = distance_calculator.get_distance(start_address, end_address)
 
 package_hash = Package.build_package_hash_from_csv()
 
-# print(package_hash[0].address)
-# print(location_hash[0].address)
+print(distance)
 
-# print(package_hash[1].address)
-# print(location_hash[1].address)
 
 
 for x in range(len(package_hash)):
